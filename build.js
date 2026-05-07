06914745e36d61a0c959d810270a0d65f00b7541
Alan Bussolini <alanbussolini@gmail.com>
2026-05-07 18:06:41 -0300

    Fix env var names

--- build.js
+++ build.js
@@ -2,12 +2,12 @@ const fs = require('fs');
 
 const env = {
   SUPABASE_URL: process.env.SUPABASE_URL,
-  SUPABASE_KEY: process.env.SUPABASE_KEY
+  SUPABASEKEY: process.env.SUPABASEKEY
 };
 
 let html = fs.readFileSync('index.html', 'utf8');
-html = html.replace("'https://dxvvusrhmaiysuooxijm.supabase.co'", `'${env.SUPABASE_URL}'`);
-html = html.replace("'sb_secret_oQEaPBHZh0uja_tq0R4ZcA_6PjqUJOo'", `'${env.SUPABASE_KEY}'`);
+html = html.replace("'{{SUPABASE_URL}}'", `'${env.SUPABASE_URL}'`);
+html = html.replace("'{{SUPABASEKEY}}'", `'${env.SUPABASEKEY}'`);
 
 fs.writeFileSync('index.html', html);
 console.log('Build done');
